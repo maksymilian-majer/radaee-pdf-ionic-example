681f73f847d6cf193d57ef888ef42de929a96861
Maksymilian Majer <1215868+maksymilian-majer@users.noreply.github.com>
2018-11-28 19:29:18 +0100

    Hiding unneeded buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,37 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
+import { HomePage } from "../pages/home/home";
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  rootPage: any = HomePage;
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
 
-      if (!platform.is('cordova')) {
+      if (!platform.is("cordova")) {
         return;
       }
+    });
 
+    this.setPdfReaderLicense().then(() => this.configurePdfReader());
+  }
+
+  async setPdfReaderLicense(): Promise<any> {
+    return new Promise((resolve, reject) => {
       RadaeePDFPlugin.activateLicense(
         {
           licenseType: 0, //0: for standard license, 1: for professional license, 2: for premium license
@@ -28,13 +39,21 @@ export class MyApp {
           email: "", //the email you entered during license activation
           key: "" //you license activation key
         },
-        function(message) { // Callback for successful opening.
-          console.log("Success: " + message);
+        resolve,
+        reject
+      );
+    });
+  }
+
+  async configurePdfReader(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      RadaeePDFPlugin.setThumbnailBGColor(
+        {
+          color: 0xFFFF0000
         },
-        function(err){ // Callback in case of error.
-          console.log("Failure: " + err);
-        });
+        resolve,
+        reject
+      );
     });
   }
 }
-
